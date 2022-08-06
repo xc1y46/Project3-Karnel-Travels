ffead7cc1d01548ce3514d23e4fa78dad36eb89b
xc1y46 <92246040+xc1y46@users.noreply.github.com>
2022-08-06 22:06:16 +0700

    Đã thêm data vào AdminRole, Admin, TransactionType, FacilityType

--- KarlanTravels.sql
+++ KarlanTravels.sql
@@ -19,8 +19,11 @@ CREATE TABLE Country(
 	RegionCode NVARCHAR(20) NOT NULL	--Ma~ vung`
 )
 
+ON [PRIMARY]
 GO
-
+INSERT INTO Country(CountryId, CountryName, Continent, RegionCode) VALUES
+(N'none', N'TempCountry', N'NoContinent', '0')
+GO
 
 IF OBJECT_ID('dbo.City', 'U') IS NOT NULL
   DROP TABLE dbo.City
@@ -29,35 +32,17 @@ CREATE TABLE City(
 	CityName NVARCHAR(255) NOT NULL,
 	CountryId NVARCHAR(40) NOT NULL,
 	PostalCode NVARCHAR(20) NOT NULL,	--Ma~ buu chinh cho shipping, refund,...
-)
-ON [PRIMARY]
-GO
 
-
-IF OBJECT_ID('dbo.Facility', 'U') IS NOT NULL
-  DROP TABLE dbo.Facility
-CREATE TABLE Facility(					-- Co so vat chat(khach san, nha` hang`,...)
-	FacilityId NVARCHAR(40) PRIMARY KEY,
-	FacilityName NVARCHAR(255) NOT NULL,
-	FacilityTypeId NVARCHAR(10) NOT NULL,	
-	FacilityLocation NVARCHAR(255) NOT NULL,
-	CityId NVARCHAR(40) NOT NULL,
-	FacilityPrice MONEY NOT NULL,
-	FacilityQuality FLOAT NOT NULL DEFAULT 1,	--Cham' diem? tu 1-10 (float)
-	Quantity INT NOT NULL DEFAULT 0,
-	FacilityAvailability BIT NOT NULL DEFAULT 0,	--Do kha? dung. hien tai (boolean)
-	DeleteFlag BIT NOT NULL DEFAULT 0	--Truong danh dau xoa' (boolean)
+	CONSTRAINT FK_CountryId FOREIGN KEY (CountryId) REFERENCES Country(CountryId)
 )
 
 ON [PRIMARY]
 GO
 
-
-INSERT INTO Facility(FacilityId, FacilityTypeId, FacilityName, FacilityLocation, CityId, FacilityPrice, FacilityQuality, Quantity, FacilityAvailability) VALUES
- (N'none',N'UNTP', N'TempFacility', N'none', N'none', 0, 0, 0, 0)		--Ban ghi rong
+INSERT INTO City(CityId, CityName, CountryId, PostalCode) VALUES
+(N'none', N'TempCity', N'none', '0')
 
 GO
-
 
 IF OBJECT_ID('dbo.FacilityType', 'U') IS NOT NULL
   DROP TABLE dbo.FacilityType
@@ -83,27 +68,6 @@ INSERT INTO FacilityType(FacilityTypeId, FacilityTypeName) VALUES
 (N'STA_DCK', N'Ships docking station'),
 (N'AIRPRT', N'Airport')
 
-
-IF OBJECT_ID('dbo.TouristSpot', 'U') IS NOT NULL
-  DROP TABLE dbo.TouristSpot
-CREATE TABLE TouristSpot(
-	TouristSpotId NVARCHAR(40) PRIMARY KEY,
-	TouristSpotName NVARCHAR(255) NOT NULL,
-	CityId NVARCHAR(40) NOT NULL,
-	CategoryId NVARCHAR(40) NOT NULL,
-	Rating FLOAT NOT NULL DEFAULT 1,	--Cham diem tu 1-10 (float)
-	TouristSpotAvailability BIT NOT NULL DEFAULT 0,
-	DeleteFlag BIT NOT NULL DEFAULT 0
-)
-
-ON [PRIMARY]
-GO
-
-
-INSERT INTO TouristSpot(TouristSpotId, TouristSpotName, CityId, CategoryId, Rating, TouristSpotAvailability) VALUES
- (N'none' ,N'TempTouristSpot', N'none', N'UNCG', 0, 0)	--Ban ghi rong
-
-
 GO
 
 
@@ -119,8 +83,143 @@ CREATE TABLE Category(
 ON [PRIMARY]
 GO
 
-INSERT INTO FacilityType(FacilityTypeId, FacilityTypeName) VALUES
+INSERT INTO Category(CategoryId, CategoryName) VALUES
 (N'UNCG', N'Uncategorized')
+
+GO
+
+
+IF OBJECT_ID('dbo.TransactionType', 'U') IS NOT NULL
+  DROP TABLE dbo.TransactionType
+CREATE TABLE TransactionType(
+	TransactionTypeId NVARCHAR(40) PRIMARY KEY,
+	TransactionTypeName NVARCHAR(40) NOT NULL,
+	TransactionDescription NVARCHAR(255) NULL,
+	TransactionPriceRate FLOAT NOT NULL DEFAULT 0,	--La` 1 he. so' nhan^ vs gia' cua tour
+)
+
+ON [PRIMARY]
+GO
+
+
+INSERT INTO TransactionType(TransactionTypeId, TransactionTypeName, TransactionDescription, TransactionPriceRate) VALUES
+(N'DEPOSIT', N'Leave a deposit', N'Leave an amount of money as a deposit', 0),	--Dat. coc.
+(N'PURCHSE', N'Purchase tour', N'Normal tour price', 1),
+(N'CANCL_EARL', N'Cancel tour early', N'No fee charges, full refund', 0),
+(N'CANCL_LATE', N'Cancel tour late', N'(Few days before tour starts) Half refund', 0.5)
+
+GO
+
+IF OBJECT_ID('dbo.AdminRole', 'U') IS NOT NULL
+  DROP TABLE dbo.AdminRole
+CREATE TABLE AdminRole(
+	RoleId NVARCHAR(40) PRIMARY KEY,
+	RoleName NVARCHAR(50) NOT NULL,
+	RoleDescription NVARCHAR(255) NULL
+)
+
+ON [PRIMARY]
+GO
+
+INSERT INTO AdminRole(RoleId, RoleName, RoleDescription) VALUES
+(N'SALE_MG',N'Saler', N'Manage sales, trasactions and user accounts, cannot access tour database'),
+(N'TOUR_MG', N'Tour Manager', N'Manage the tour database, cannot access sales and transactions records'),
+(N'SN_MG', N'Senior Maneger', N'Have access to all divisions, including admin accounts')
+
+GO
+
+
+IF OBJECT_ID('dbo.Customer', 'U') IS NOT NULL
+  DROP TABLE dbo.Customer
+CREATE TABLE Customer(
+	CustomerId INT PRIMARY KEY IDENTITY (1,1),
+	Username NVARCHAR(255) NOT NULL,
+	Email NVARCHAR(255) NOT NULL,
+	Phone NVARCHAR(20) NOT NULL,
+	CityId NVARCHAR(40) NOT NULL,
+	UserPassword NVARCHAR(20) NOT NULL,	
+	DeleteFlag BIT NOT NULL DEFAULT 0,
+
+	CONSTRAINT FK_CityId1 FOREIGN KEY (CityId) REFERENCES City(CityId)
+)
+
+GO
+
+
+IF OBJECT_ID('dbo.[Admin]', 'U') IS NOT NULL
+  DROP TABLE dbo.[Admin]
+CREATE TABLE [Admin](
+	AdminId INT PRIMARY KEY IDENTITY (1,1),
+	AdminName NVARCHAR(30) NOT NULL,
+	AdminPassword NVARCHAR(255) NOT NULL,
+	RoleId NVARCHAR(40) NOT NULL FOREIGN KEY REFERENCES AdminRole(RoleId),
+	DeleteFlag BIT NOT NULL DEFAULT 0,
+
+	CONSTRAINT FK_RoleId FOREIGN KEY (RoleId) REFERENCES AdminRole(RoleId)
+)
+
+ON [PRIMARY]
+GO
+
+
+INSERT INTO [Admin](AdminName, AdminPassword ,RoleId) VALUES
+(N'NTA', N'40bd001563085fc35165329ea1ff5c5ecbdbbeef', N'SN_MG'),	--Pass 123
+(N'VHL', N'51eac6b471a284d3341d8c0c63d0f1a286262a18', N'SALE_MG'),	--Pass 456
+(N'VCM', N'fc1200c7a7aa52109d762a9f005b149abef01479', N'TOUR_MG')	--Pass 789
+
+GO
+
+
+IF OBJECT_ID('dbo.Facility', 'U') IS NOT NULL
+  DROP TABLE dbo.Facility
+CREATE TABLE Facility(					-- Co so vat chat(khach san, nha` hang`,...)
+	FacilityId NVARCHAR(40) PRIMARY KEY,
+	FacilityName NVARCHAR(255) NOT NULL,
+	FacilityTypeId NVARCHAR(40) NOT NULL,	
+	FacilityLocation NVARCHAR(255) NOT NULL,
+	CityId NVARCHAR(40) NOT NULL,
+	FacilityPrice MONEY NOT NULL,
+	FacilityQuality FLOAT NOT NULL DEFAULT 1,	--Cham' diem? tu 1-10 (float)
+	Quantity INT NOT NULL DEFAULT 0,
+	FacilityAvailability BIT NOT NULL DEFAULT 0,	--Do kha? dung. hien tai (boolean)
+	DeleteFlag BIT NOT NULL DEFAULT 0	--Truong danh dau xoa' (boolean)
+
+	CONSTRAINT FK_FacilityTypeId FOREIGN KEY (FacilityTypeId) REFERENCES FacilityType(FacilityTypeId),
+	CONSTRAINT FK_CityId2 FOREIGN KEY (CityId) REFERENCES City(CityId)
+)
+
+ON [PRIMARY]
+GO
+
+
+INSERT INTO Facility(FacilityId, FacilityTypeId, FacilityName, FacilityLocation, CityId, FacilityPrice, FacilityQuality, Quantity, FacilityAvailability) VALUES
+ (N'none',N'UNTP', N'TempFacility', N'none', N'none', 0, 0, 0, 0)		--Ban ghi rong
+
+GO
+
+IF OBJECT_ID('dbo.TouristSpot', 'U') IS NOT NULL
+  DROP TABLE dbo.TouristSpot
+CREATE TABLE TouristSpot(
+	TouristSpotId NVARCHAR(40) PRIMARY KEY,
+	TouristSpotName NVARCHAR(255) NOT NULL,
+	CityId NVARCHAR(40) NOT NULL,
+	CategoryId NVARCHAR(40) NOT NULL,
+	Rating FLOAT NOT NULL DEFAULT 1,	--Cham diem tu 1-10 (float)
+	TouristSpotAvailability BIT NOT NULL DEFAULT 0,
+	DeleteFlag BIT NOT NULL DEFAULT 0
+
+	CONSTRAINT FK_CategoryId FOREIGN KEY (CategoryId) REFERENCES Category(CategoryId),
+	CONSTRAINT FK_CityId3 FOREIGN KEY (CityId) REFERENCES City(CityId)
+
+)
+
+ON [PRIMARY]
+GO
+
+
+INSERT INTO TouristSpot(TouristSpotId, TouristSpotName, CityId, CategoryId, Rating, TouristSpotAvailability) VALUES
+ (N'none' ,N'TempTouristSpot', N'none', N'UNCG', 0, 0)	--Ban ghi rong
+
 
 GO
 
@@ -146,12 +245,16 @@ CREATE TABLE TourDetails(
 	TourDetailsId NVARCHAR(40) PRIMARY KEY,
 	TourId NVARCHAR(40) NOT NULL,
 	TourDetailsName NVARCHAR(255) NOT NULL,
+	TouristSpotId NVARCHAR(40) NOT NULL DEFAULT 0 FOREIGN KEY REFERENCES TouristSpot(TouristSpotId),
+	FacilityId NVARCHAR(40) NOT NULL DEFAULT 0 FOREIGN KEY REFERENCES Facility(FacilityId),
 	Activity NVARCHAR(100) NOT NULL,
 	ActivityTime NVARCHAR(30) NOT NULL,
-	FacilityId NVARCHAR(40) NOT NULL DEFAULT 0,
-	TouristSpotsId NVARCHAR(40) NOT NULL DEFAULT 0,
 	TourNote NVARCHAR(255) NULL,
 	DeleteFlag BIT NOT NULL DEFAULT 0
+
+	CONSTRAINT FK_TourId1 FOREIGN KEY (TourId) REFERENCES Tour(TourId),
+	CONSTRAINT FK_TouristSpotId FOREIGN KEY (TouristSpotId) REFERENCES TouristSpot(TouristSpotId),
+	CONSTRAINT FK_FacilityId FOREIGN KEY (FacilityId) REFERENCES Facility(FacilityId)
 )
 
 GO
@@ -160,87 +263,16 @@ IF OBJECT_ID('dbo.TransactionsRecords', 'U') IS NOT NULL
   DROP TABLE dbo.TransactionsRecords	--Ban ghi cac giao dich
 CREATE TABLE TransactionsRecords(
 	RecordId NVARCHAR(40) PRIMARY KEY,
-	TransactionTypeId INT NOT NULL,
+	TransactionTypeId NVARCHAR(40) NOT NULL,
 	TourId NVARCHAR(40) NOT NULL,
 	CustomerID INT NOT NULL,	--Nguoi mua
 	AdminID INT NOT NULL,		--Nguoi edit ban ghi
 	DeleteFlag BIT NOT NULL DEFAULT 0
+
+	CONSTRAINT FK_TransactionTypeId FOREIGN KEY (TransactionTypeId) REFERENCES TransactionType(TransactionTypeId),
+	CONSTRAINT FK_TourId2 FOREIGN KEY (TourId) REFERENCES Tour(TourId),
+	CONSTRAINT FK_CustomerID FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID),
+	CONSTRAINT FK_AdminID FOREIGN KEY (AdminID) REFERENCES [Admin](AdminID)
 )
-
-GO
-
-IF OBJECT_ID('dbo.TransactionType', 'U') IS NOT NULL
-  DROP TABLE dbo.TransactionType
-CREATE TABLE TransactionType(
-	TransactionTypeId NVARCHAR(40) PRIMARY KEY,
-	TransactionTypeName NVARCHAR(40) NOT NULL,
-	TransactionDescription NVARCHAR(255) NULL,
-	TransactionPriceRate FLOAT NOT NULL DEFAULT 0,	--La` 1 he. so' nhan^ vs gia' cua tour
-)
-
-ON [PRIMARY]
-GO
-
-
-INSERT INTO TransactionType(TransactionTypeId, TransactionTypeName, TransactionDescription, TransactionPriceRate) VALUES
-(N'DEPOSIT', N'Leave a deposit', N'Leave an amount of money as a deposit', 0),	--Dat. coc.
-(N'PURCHSE', N'Purchase tour', N'Normal tour price', 1),
-(N'CANCL_EARL', N'Cancel tour early', N'No fee charges, full refund', 0),
-(N'CANCL_LATE', N'Cancel tour late', N'(Few days before tour starts) Half refund', 0.5)
-GO
-
-
-IF OBJECT_ID('dbo.Customer', 'U') IS NOT NULL
-  DROP TABLE dbo.Customer
-CREATE TABLE Customer(
-	CustomerId INT PRIMARY KEY IDENTITY (1,1),
-	Username NVARCHAR(255) NOT NULL,
-	Email NVARCHAR(255) NOT NULL,
-	Phone NVARCHAR(20) NOT NULL,
-	CityId INT NOT NULL,
-	UserPassword NVARCHAR(20) NOT NULL,	
-	DeleteFlag BIT NOT NULL DEFAULT 0
-)
-
-GO
-
-
-IF OBJECT_ID('dbo.[Admin]', 'U') IS NOT NULL
-  DROP TABLE dbo.[Admin]
-CREATE TABLE [Admin](
-	AdminId INT PRIMARY KEY IDENTITY (1,1),
-	AdminName NVARCHAR(30) NOT NULL,
-	AdminPassword NVARCHAR(255) NOT NULL,
-	RoleId NVARCHAR(40) NOT NULL,
-	DeleteFlag BIT NOT NULL DEFAULT 0
-)
-
-ON [PRIMARY]
-GO
-
-
-INSERT INTO [Admin](AdminName, AdminPassword ,RoleId) VALUES
-(N'NTA', N'40bd001563085fc35165329ea1ff5c5ecbdbbeef', N'SN_MG'),	--Pass 123
-(N'VHL', N'51eac6b471a284d3341d8c0c63d0f1a286262a18', N'SALE_MG'),	--Pass 456
-(N'VCM', N'fc1200c7a7aa52109d762a9f005b149abef01479', N'TOUR_MG')	--Pass 789
-
-GO
-
-
-IF OBJECT_ID('dbo.AdminRole', 'U') IS NOT NULL
-  DROP TABLE dbo.AdminRole
-CREATE TABLE AdminRole(
-	RoleId NVARCHAR(40) PRIMARY KEY,
-	RoleName NVARCHAR(50) NOT NULL,
-	RoleDescription NVARCHAR(255) NULL
-)
-
-ON [PRIMARY]
-GO
-
-INSERT INTO AdminRole(RoleId, RoleName, RoleDescription) VALUES
-(N'SALE_MG',N'Saler', N'Manage sales, trasactions and user accounts, cannot access tour database'),
-(N'TOUR_MG', N'Tour Manager', N'Manage the tour database, cannot access sales and transactions records'),
-(N'SN_MG', N'Senior Maneger', N'Have access to all divisions, including admin accounts')
 
 GO
